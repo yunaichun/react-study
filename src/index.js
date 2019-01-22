9043cd6f8ad90e5b20a8083f7c43f31f51526af0
余乃春 <yunaichun@hikvision.com.cn>
2019-01-22 08:39:16 +0800

    修改应用状态的专有方法：dispatch函数

--- src/index.js
+++ src/index.js
@@ -10,17 +10,29 @@ const appState = {
   }
 };
 
+
 /**
- * [renderApp 主渲染函数]
- * @param  {[type]} appState [应用状态]
+ * [dispatch 修改应用状态的专用方法：dispatch]
+ * @param  {[Object]} action [修改应用状态的指令：包括 type 和 payload]
+ * @return {[type]}          [返回最新的应用状态]
  */
-function renderApp(appState) {
-  renderTitle(appState.title);
-  renderContent(appState.content);
+function dispatch(action) {
+  switch (action.type) {
+    case 'UPDATE_TITLE_TEXT':
+      appState.title.text = action.text
+      break
+    case 'UPDATE_TITLE_COLOR':
+      appState.title.color = action.color
+      break
+    default:
+      break
+  }
 }
+
+
 /**
- * [renderTitle 渲染title]
- * @param  {[type]} title [应用状态]
+ * [renderTitle 渲染 title ]
+ * @param  {[Object]} title [修改的数据对象]
  */
 function renderTitle(title) {
   const titleDOM = document.getElementById('title');
@@ -28,13 +40,29 @@ function renderTitle(title) {
   titleDOM.style.color = title.color;
 }
 /**
- * [renderContent 渲染content]
- * @param  {[type]} content [应用状态]
+ * [renderContent 渲染 content ]
+ * @param  {[Object]} content [修改的数据对象]
  */
 function renderContent(content) {
   const contentDOM = document.getElementById('content');
   contentDOM.innerHTML = content.text;
   contentDOM.style.color = content.color;
 }
+/**
+ * [renderApp 主渲染函数]
+ * @param  {[Object]} appState [应用状态]
+ */
+function renderApp(appState) {
+  renderTitle(appState.title);
+  renderContent(appState.content);
+}
 
+
+/*首次渲染页面*/
+renderApp(appState);
+/*修改标题文本*/
+dispatch({ type: 'UPDATE_TITLE_TEXT', text: '《React.js 小书》' });
+/*修改标题颜色*/
+dispatch({ type: 'UPDATE_TITLE_COLOR', color: 'blue' });
+/*把新的数据渲染到页面上*/
 renderApp(appState);
