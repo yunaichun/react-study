a862e2178c314a979745b47d5a7187317a8d8c66
yunaichun <naichun.yu@cootek.cn>
2019-04-16 15:40:26 +0800

    redux-promise源码上传

---
+++ redux-promise-src/index.js
@@ -0,0 +1,19 @@
+import isPromise from 'is-promise';
+import { isFSA } from 'flux-standard-action';
+
+export default function promiseMiddleware({ dispatch }) {
+  return next => action => {
+    if (!isFSA(action)) {
+      return isPromise(action) ? action.then(dispatch) : next(action);
+    }
+
+    return isPromise(action.payload)
+      ? action.payload
+          .then(result => dispatch({ ...action, payload: result }))
+          .catch(error => {
+            dispatch({ ...action, payload: error, error: true });
+            return Promise.reject(error);
+          })
+      : next(action);
+  };
+}
