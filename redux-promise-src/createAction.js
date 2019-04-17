7ff2751f9331c6fb4d09068e0be444f1230f2d31
yunaichun <naichun.yu@cootek.cn>
2019-04-17 21:49:43 +0800

    简化redux-actions源码

--- redux-promise-src/createAction.js
+++ redux-promise-src/createAction.js
@@ -1,47 +1,43 @@
-import invariant from 'invariant';
-import isFunction from './utils/isFunction';
-import identity from './utils/identity';
-import isNull from './utils/isNull';
+// site: https://github.com/redux-utilities/redux-actions/blob/master/src/createAction.js
 
-export default function createAction(
-  type,
-  payloadCreator = identity,
-  metaCreator
-) {
-  invariant(
-    isFunction(payloadCreator) || isNull(payloadCreator),
-    'Expected payloadCreator to be a function, undefined or null'
-  );
+/**
+ * 参数不是function调用此函数
+ */
+function identity(t) {
+    return t;
+}
 
-  const finalPayloadCreator =
-    isNull(payloadCreator) || payloadCreator === identity
-      ? identity
-      : (head, ...args) =>
-          head instanceof Error ? head : payloadCreator(head, ...args);
-
-  const hasMeta = isFunction(metaCreator);
-  const typeString = type.toString();
-
-  const actionCreator = (...args) => {
-    const payload = finalPayloadCreator(...args);
-    const action = { type };
-
-    if (payload instanceof Error) {
-      action.error = true;
-    }
-
-    if (payload !== undefined) {
-      action.payload = payload;
-    }
-
-    if (hasMeta) {
-      action.meta = metaCreator(...args);
-    }
-
-    return action;
-  };
-
-  actionCreator.toString = () => typeString;
-
-  return actionCreator;
+/**
+ * createAction 创建action
+ * @param type  action的类型
+ * @param actionCreator 需要创建的action,函数
+ * @param metaCreator   action的属性
+ * @returns {Function}
+ */
+export default function createAction(type, actionCreator, metaCreator) {
+    /**
+     * finalActionCreator最终创建的action,
+     * 判断传进来的参数是不是function,true返回这个函数,false调用identity函数
+     */
+    const finalActionCreator = typeof actionCreator === 'function' ?
+        actionCreator :
+        identity;
+    /*返回一个匿名函数*/
+    return (...args) => {
+        /*创建的action,只有两个属性*/
+        const action = {
+            type,
+            payload: finalActionCreator(...args)
+        };
+        /*如果给匿名函数传递参数的长度为1个,或者第一个参数元素的类型为Error,那么这么action的error属性为true*/
+        if (args.length === 1 && args[0] instanceof Error) {
+            // Handle FSA errors where the payload is an Error object. Set error.
+            action.error = true;
+        }
+        /*传递到action里面的函数*/
+        if (typeof metaCreator === 'function') {
+            action.meta = metaCreator(...args);
+        }
+        return action;
+    };
 }
