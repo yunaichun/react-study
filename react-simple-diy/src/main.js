cec00021dbf913d4871f7e79b1d3d25113b7755a
yunaichun <yunaichun@qingting.fm>
2020-08-21 17:48:34 +0800

    fix: 重新梳理 createElement 方法

--- react-simple-diy/src/main.js
+++ react-simple-diy/src/main.js
@@ -1,30 +1,57 @@
- import {createElement, Component, render} from '../lib/simple-react';
+ import { createElement, Component, render} from '../lib/simple-react';
+
  class MyComponent extends Component {
 
     render() {
-        return <div>
+        return <div id="wrap">
             <h1>my component</h1>
             {this.children}
         </div>
     }
 }
 
-render(<MyComponent id='a' class='b'>
-    <div>1</div>
-    <div>2</div>
-    <div>3</div>
+render(<MyComponent>
+    <div id='1' class='class1'>1</div>
+    <div id='2'>2</div>
+    <div id='3'><div>3</div></div>
  </MyComponent>, document.getElementById('app'));
 
 
 // == JSX 原理
-// let a = <div id="a" class='b'>
-//     <div>1</div>
-//     <div>2</div>
-//     <div>3</div>
-// </div>
+// let a = <MyComponent>
+// <div id='1' class='class1'>1</div>
+// <div id='2'>2</div>
+// <div id='3'><div>3</div></div>
+// </MyComponent>
 // == @babel/plugin-transform-react-jsx 会将 jsx 翻译成下面的语句
 // == 由于未引入 react ，所以 createElement 会显示未定义。我们自己实现 JSX 的解析
-// var a = createElement("div", {
-//     id: "a",
-//     "class": "b"
-// }, createElement("div", null, "1"), createElement("div", null, "2"), createElement("div", null, "3"));
+// var a = createElement(
+//     MyComponent,
+//     null,
+//     createElement(
+//         "div", 
+//         {
+//             id: "1",
+//             "class": "class1",
+//         },
+//         "1",
+//     ),
+//     createElement(
+//         "div",
+//         {
+//             id: "2",
+//         },
+//         "2",
+//     ),
+//     createElement(
+//         "div",
+//         {
+//             id: "3",
+//         },
+//         createElement(
+//             "div",
+//             null,
+//             "3",
+//         ),
+//     ),
+// );
