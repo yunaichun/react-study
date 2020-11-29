49a16fa7eebcf9c0e09d74fb699cf77adc1d44ed
yunaichun <yunaichun@qingting.fm>
2020-11-29 13:03:23 +0800

    feat: createElemen

---
+++ react-simple-diy2/src/2.createElement/index.js
@@ -0,0 +1,81 @@
+// == 一. React 代码实现的 jsx 解析 和 render 函数
+// import React, { createElement as createElemenDiy } from 'react';
+// import ReactDOM from 'react-dom';
+// const element = (
+//   <div id="foo">
+//     <a>bar</a>
+//     <b />
+//   </div>
+// );
+// const container = document.getElementById('root');
+// ReactDOM.render(element, container);
+
+// == 二. 实现自己的 createElemenDiy 函数
+// == 2.1 通过延展符使得 children 在 props 中为数组
+function createElemenDiy(type, props, ...children) {
+  return {
+    type,
+    props: {
+      ...props,
+      children: children.map(child =>
+        typeof child === "object"
+          ? child
+          : createTextElement(child)
+      ),
+    },
+  }
+}
+// == 2.2 子元素可能是 TEXT_ELEMENT
+function createTextElement(text) {
+  return {
+    type: "TEXT_ELEMENT",
+    props: {
+      nodeValue: text,
+      children: [],
+    },
+  }
+}
+// == createElemenDiy 函数调用
+// const element = createElemenDiy(
+//   "div",
+//   { id: "foo" },
+//   createElemenDiy("a", null, "bar"),
+//   createElemenDiy("b")
+// );
+// == 等价于 jsx 的写法
+const element = (
+  <div id="foo">
+    <a>bar</a>
+    <b />
+  </div>
+);
+// == 最后 element 返回一个 js 对象如下所示
+console.log(element);
+// {
+//   "type": "div",
+//   "props": {
+//     "id": "foo",
+//     "children": [
+//       {
+//         "type": "a",
+//         "props": {
+//           "children": [
+//             {
+//               "type": "TEXT_ELEMENT",
+//               "props": {
+//                 "nodeValue": "bar",
+//                 "children": []
+//               }
+//             }
+//           ]
+//         }
+//       },
+//       {
+//         "type": "b",
+//         "props": {
+//           "children": []
+//         }
+//       }
+//     ]
+//   }
+// }
