b8f6453e1ecd264d4aece41978569a4e8873abf9
yunaichun <yunaichun@qingting.fm>
2020-12-20 19:57:22 +0800

    feat: modify

--- react-simple/simple-two/src/2.createElement/index.js
+++ react-simple/simple-two/src/2.createElement/index.js
@@ -1,17 +1,10 @@
-// == 一. React 代码实现的 jsx 解析 和 render 函数
-// import React, { createElement as simpleCreateElement } from 'react';
-// import ReactDOM from 'react-dom';
-// const element = (
-//   <div id="foo">
-//     <a>bar</a>
-//     <b />
-//   </div>
-// );
-// const container = document.getElementById('root');
-// ReactDOM.render(element, container);
+// 由上一节可知 simpleCreateElement 本质:
+// 1、参数: type、props、children 属性
+// 2、返回: js 对象 - { type, children: [{ type, ... }] }
 
-// == 二. 实现自己的 simpleCreateElement 函数
-// == 2.1 通过延展符使得 children 在 props 中为数组
+
+// == 实现自己的 simpleCreateElement 函数
+// == 1、通过延展符使得 children 在 props 中为数组
 export default function simpleCreateElement(type, props, ...children) {
   return {
     type,
@@ -25,7 +18,8 @@ export default function simpleCreateElement(type, props, ...children) {
     },
   }
 }
-// == 2.2 子元素可能是 TEXT_ELEMENT
+
+// == 2、子元素可能是 TEXT_ELEMENT
 function createTextElement(text) {
   return {
     type: 'TEXT_ELEMENT',
@@ -35,21 +29,21 @@ function createTextElement(text) {
     },
   }
 }
-// == simpleCreateElement 函数调用
-// const element = simpleCreateElement(
-//   "div",
-//   { id: "foo" },
-//   simpleCreateElement("a", null, "bar"),
-//   simpleCreateElement("b")
-// );
-// == 等价于 jsx 的写法
+
+// == 3、结果验证
 const element = (
   <div id="foo">
     <a>bar</a>
     <b />
   </div>
 );
-// == 最后 element 返回一个 js 对象如下所示
+// == 等价于
+// const element = simpleCreateElement(
+//   "div",
+//   { id: "foo" },
+//   simpleCreateElement("a", null, "bar"),
+//   simpleCreateElement("b")
+// );
 console.log(1111, element);
 // {
 //   "type": "div",
